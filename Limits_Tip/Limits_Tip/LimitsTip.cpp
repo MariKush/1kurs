9ea9102e94987376f462f9823cca028bd8f2c321
MariKush <43176370+MariKush@users.noreply.github.com>
2018-09-25 20:58:25 +0300

    лимиты типов

---
+++ Limits_Tip/Limits_Tip/LimitsTip.cpp
@@ -0,0 +1,29 @@
+// LimitsTip.cpp: перевіряє границі інтервалу
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+
+int main()
+{
+	int n_int = INT_MAX;
+	short n_short = SHRT_MAX;
+	long n_long = LONG_MAX;
+
+	cout << "int is " << sizeof(int) << " bytes." << endl;
+	cout << "short is " << sizeof n_short << " bytes." << endl;
+	cout << "long is " << sizeof n_long << " bytes." << endl << endl;
+	cout << "Maximum values:" << endl;
+	cout << "int: " << n_int << endl;
+	cout << "short: " << n_short << endl;
+	cout << "long: " << n_long << endl << endl;
+	cout << "Minimum int value = " << INT_MIN << endl;
+	cout << "Bits per byte = " << CHAR_BIT << endl;
+
+	return 0;
+}
+
